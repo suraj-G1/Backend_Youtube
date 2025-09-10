c1e600b645e3224a5b3d2f1c244236f6e7dafe5d
Suraj Gund <surajgund46@gmail.com>
2025-09-10 18:34:44 +0530

    added : cloudinary to file upload

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,24 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadToCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("File has been uploaded to Cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlickSync(localFilePath);
+    return null;
+  }
+};
+
+module.exports = { uploadToCloudinary };
